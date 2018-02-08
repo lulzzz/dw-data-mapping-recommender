f32431fb2651bd3c13cc3d4551c05caa7872fb78
martin.rey <marto.rey2006@gmail.com>
2018-02-08 14:46:36 -0800

    - updated to use similarity instead of distances - softmax distribution for the output

--- src/recommender/recommender.py
+++ src/recommender/recommender.py
@@ -1,25 +1,43 @@
 import gensim
 import collections
+import numpy as np
+from gensim.similarities import WmdSimilarity
 
 
 class Recommender(object):
     def __init__(self):
         self.model = gensim.models.KeyedVectors.load_word2vec_format('../datasets/GoogleNews-vectors-negative300.bin',
                                                                      binary=True)
+        self.model.init_sims(replace=True)
 
     def recommend(self, sentence, recommendations):
-        distances = []
+        similarity_distances = []
         recomendation_result = collections.defaultdict(dict)
-        for word_to_recommend in recommendations:
-            distances.append(
-                self._calculate_distance(first_sentence=sentence,
-                                         second_sentence=word_to_recommend))
-        for word, probability in zip(recommendations, distances):
+
+        similarity_distances.extend(
+            self.softmax(self._calculate_similarity_distance(sentence=sentence, corpus=recommendations)).tolist())
+
+        for word, probability in zip(recommendations, similarity_distances):
             recomendation_result[sentence][word] = probability
 
         return recomendation_result
 
-    def _calculate_distance(self, first_sentence, second_sentence):
+    def _calculate_similarity_distance(self, sentence, corpus):
+        similarity_query_response = WmdSimilarity(corpus, self.model, normalize_w2v_and_replace=False)
+        similarities = similarity_query_response[sentence]
+
+        # for i in len(similarities):
+        #     print('sim = %.4f' % similarities[i][1])
+        #     result.append(corpus[similarities[i][0]])
+        #     result.append(similarities[i][1])
+
+        return similarities
+
+    def _calculate_raw_distance(self, first_sentence, second_sentence):
         distance = self.model.wmdistance(first_sentence, second_sentence)
 
         return distance
+
+    def softmax(self, x):
+        """Compute softmax values for each sets of scores in x."""
+        return np.exp(x) / np.sum(np.exp(x), axis=0)
